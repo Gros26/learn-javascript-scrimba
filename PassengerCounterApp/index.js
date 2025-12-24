943dbbda2d80976d08f35792241f9ab42d7867c8
zoikle <134870626+Gros26@users.noreply.github.com>
2025-12-24 01:48:47 -0500

    on click event was added and their respective function

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,5 +1,12 @@
 // document.getElementById("count-el").innerText = 5
 
 let count = 0
-console.log(count)
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+function incrementButton() {
+    count += 1
+    countEl.innerText = count
+}
 
