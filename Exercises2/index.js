0e23e59205b46ccae194141e3bdffa29f6e3babd
zoikle <134870626+Gros26@users.noreply.github.com>
2025-12-29 21:45:11 -0500

    first exercise

---
+++ Exercises2/index.js
@@ -0,0 +1,14 @@
+let person = {
+    name: "Gros",
+    age: 10,
+    country: "Colombia",
+    
+}
+
+function logData() {
+    console.log(person.name + " is " + person.age + " yeras old and lives in " + person.country)
+}
+
+
+///////////
+
