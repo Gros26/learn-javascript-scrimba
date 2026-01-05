203e1a42a086f5c153817f2479a9dd2b8a62dfe1
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-05 18:16:42 -0500

    use innerHTML to create a li element

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,10 +1,17 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    printArray(myLeads)
 })
 
+
+function printArray(array) {
+    ulEl.textContent = ""
+    array.forEach(e => ulEl.innerHTML += "<li>" + e + "</li>")
+}
