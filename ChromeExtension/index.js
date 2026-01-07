0f280b0f94b51492b33c4f26b2697bc099816b48
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-07 13:28:35 -0500

    added tab-btn

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -3,7 +3,12 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+const tabs = [
+    {url: "https://www.google.com/maps/place/Metros+Pan+Panader%C3%ADa+Y+Pasteler%C3%ADa/@3.4646001,-76.5258643,13.48z/data=!4m8!3m7!1s0x8e30a3d8231056a7:0x6d87b0e6100e6b43!8m2!3d3.3756011!4d-76.5505143!9m1!1b1!16s%2Fg%2F11dfq6zt15?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D"}
+]
 
 
 if (leadsFromLocalStorage) {
@@ -14,7 +19,6 @@ if (leadsFromLocalStorage) {
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    console.log(localStorage.getItem("myLeads"))
     renderLeads(myLeads)
     inputEl.value = ""
 })
@@ -23,6 +27,13 @@ inputBtn.addEventListener("click", function() {
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
+    renderLeads(myLeads)
+})
+
+
+tabBtn.addEventListener("click", function() {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads(myLeads)
 })
 
