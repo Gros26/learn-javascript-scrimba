a13ca93be8e3f9bb4b6050e12fccb6da373e15e5
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-07 02:40:31 -0500

    Now we retrieve the data from the localStorage

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -2,7 +2,13 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
