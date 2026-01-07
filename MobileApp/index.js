bd3302382b54302566b5ae1d658f7916b4ca8e98
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-07 17:08:59 -0500

    initilized mobile app

---
+++ MobileApp/index.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+import { firebaseConfig } from "./config.js";
+
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+console.log(app)
+console.log(firebaseConfig.databaseURL)
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
+    inputEl.value = ""
+})
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
+
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
+})
+
+
+function renderLeads(array) {
+    let listItems = ""
+    // array.forEach(e => listItems += "<li>" + "<a href='" + e +"' target='_blank'>" + e + "</a>" + "</li>")
+    array.forEach(e => listItems += 
+        `<li>
+            <a href='${e}' target='_blank'>
+                ${e}
+            </a>
+        </li>`)
+    ulEl.innerHTML = listItems
+}
+
