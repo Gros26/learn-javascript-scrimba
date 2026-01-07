e7abc4a5b94b7959cc01841eceda5fca0a18d415
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-07 18:06:36 -0500

    push leads to firebase

--- MobileApp/index.js
+++ MobileApp/index.js
@@ -1,48 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+import { getDatabase,
+         ref,
+         push } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 import { firebaseConfig } from "./config.js";
 
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
-console.log(app)
-console.log(firebaseConfig.databaseURL)
-
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const refDB = ref(database, "leadsTracker")
 
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads(myLeads)
-}
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads(myLeads)
+    push(refDB, inputEl.value)
+    console.log(inputEl.value)
     inputEl.value = ""
 })
 
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    renderLeads(myLeads)
-})
-
-
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLeads(myLeads)
-    })
 })
 
 
