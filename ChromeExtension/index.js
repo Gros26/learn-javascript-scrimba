c907a4029b1ddd86a11ec946a6892d2c93ef1776
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-07 14:13:55 -0500

    finished chrome extension app

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -6,10 +6,6 @@ const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-const tabs = [
-    {url: "https://www.google.com/maps/place/Metros+Pan+Panader%C3%ADa+Y+Pasteler%C3%ADa/@3.4646001,-76.5258643,13.48z/data=!4m8!3m7!1s0x8e30a3d8231056a7:0x6d87b0e6100e6b43!8m2!3d3.3756011!4d-76.5505143!9m1!1b1!16s%2Fg%2F11dfq6zt15?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D"}
-]
-
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -32,9 +28,11 @@ deleteBtn.addEventListener("dblclick", function() {
 
 
 tabBtn.addEventListener("click", function() {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads(myLeads)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
 })
 
 
