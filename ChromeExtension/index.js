2d8798e5a354ff4152e9acbd30f316674cf20885
zoikle <134870626+Gros26@users.noreply.github.com>
2026-01-07 12:54:20 -0500

    make the deletebe button work

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -2,6 +2,7 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
@@ -18,6 +19,12 @@ inputBtn.addEventListener("click", function() {
     inputEl.value = ""
 })
 
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
 
 
 function renderLeads(array) {
